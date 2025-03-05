63378fb7bd9bfdf7479e93be8eebdce04d4eed42
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2025-03-05 22:33:24 +0700

    build: update prettier and dependencies
    - Updated prettier to v3.5.3
    - Updated prettier-plugin-tailwindcss
    - Removed unused import in Header.tsx
    - Updated menuData.tsx
    - Minor adjustments to Header.tsx

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -3,30 +3,24 @@ import { Menu } from "@/types/menu";
 const menuData: Menu[] = [
   {
     id: 1,
-    title: "Home",
-    path: "/",
-    newTab: false,
-  },
-  {
-    id: 2,
     title: "About",
     path: "/about",
     newTab: false,
   },
   {
-    id: 3,
+    id: 2,
     title: "Pricing",
     path: "/pricing",
     newTab: false,
   },
   {
-    id: 5,
+    id: 3,
     title: "Contact",
     path: "/contact",
     newTab: false,
   },
   {
-    id: 5,
+    id: 4,
     title: "Blog",
     path: "/blogs",
     newTab: false,
